d30eb4f080a9510809d9574098e4fc76d067fed1
Santiago Quintero <squin@umich.edu>
2020-01-31 23:07:57 -0500

    python

---
+++ python/forloops.py
@@ -0,0 +1,13 @@
+for i in range(3):
+	cmd = input('Enter command: ')
+	if cmd == 'break':
+		break
+	elif cmd == 'pass':
+		pass
+		print('command is pass')
+	elif cmd == 'continue'
+		continue print('command is continue')
+
+
+
+input('Press ENTER to exit')
---
+++ python/guess_lucky_number.py
@@ -0,0 +1,11 @@
+import random
+
+def guess_lucky_number()
+	colors = ['Yellow','Blue','Red','Orange','Purple','Green']
+	lucky_color = random.choice(colors)
+	print(colors)
+
+guess_lucky_number()
+
+
+input('Press ENTER to exit')
